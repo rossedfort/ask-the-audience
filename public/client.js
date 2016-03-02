29629da8b3543b02ff74ca12a29d34d09f6e8f20
rossedfort <rossedfort@gmail.com>
2016-03-02 09:26:27 -0700

    finish code-along segment of tutorial

---
+++ public/client.js
@@ -0,0 +1,23 @@
+var socket = io();
+
+var connectionCount = document.getElementById('connection-count');
+var statusMessage = document.getElementById('status-message');
+var buttons = document.querySelectorAll('#choices button');
+
+socket.on('usersConnected', function (count) {
+  connectionCount.innerText = 'Connected Users: ' + count;
+});
+
+socket.on('statusMessage', function (message) {
+  statusMessage.innerText = message;
+});
+
+socket.on('voteCount', function (votes) {
+  console.log(votes);
+});
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+    socket.send('voteCast', this.innerText);
+  });
+}
